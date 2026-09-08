b50dbf97d887ad2b47cd22b4c204f2194738d40c
Mariana Beatriz Vital de Moura <20231204010036@ifrn.local>
2026-09-08 16:07:09 -0300

    Criando repository e tudo mais

--- src/main/java/ifrn/pi/eventos/controllers/EventosControllers.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosControllers.java
@@ -1,13 +1,18 @@
 package ifrn.pi.eventos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ifrn.pi.eventos.models.Evento;
+import ifrn.pi.eventos.repositories.EventoRepository;
 
 @Controller
 public class EventosControllers {
+	
+	@Autowired
+	private EventoRepository er;
 	
 	@RequestMapping("/eventos/form")
 	public String form() {
@@ -26,6 +31,14 @@ public class EventosControllers {
 		return "home";
 		
 		
+	}
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento) {
+		
+		System.out.println(evento);
+		er.save(evento);
+		
+		return "evento-adicionado";
 	}
 
 }
--- src/main/java/ifrn/pi/eventos/models/Evento.java
+++ src/main/java/ifrn/pi/eventos/models/Evento.java
@@ -14,6 +14,12 @@ public class Evento {
 	@Id
 	@GeneratedValue(strategy =GenerationType.IDENTITY )
 	private Long id;
+	@Override
+	public String toString() {
+		return "Evento [id=" + id + ", nome=" + nome + ", local=" + local + ", data=" + data + ", horario=" + horario
+				+ "]";
+	}
+
 	private String nome;
 	private String local;
 	private String data;
